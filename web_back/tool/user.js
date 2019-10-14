0b5d516cec1cf20ff639c8bcee38680404e0ad7a
shenchunfang <1224768086@qq.comm>
2019-10-14 10:44:37 +0800

    新增登出功能和主页修改用户信息功能

--- web_back/tool/user.js
+++ web_back/tool/user.js
@@ -1,6 +1,5 @@
 var user = {
     login: function (options) {
-        console.log(options)
         $.ajax({
             type: 'post',
             url: USER_LOGIN,
@@ -8,5 +7,39 @@ var user = {
             success:options.callback
 
         })
-    }
+    },
+
+    logout:function(options){
+        $.ajax({
+            type:'post',
+            url:USER_LOGOUT,
+            success:options.callback
+        })
+    },
+
+    getuser:function(options){
+        $.ajax({
+            url:USER_GETUSER,
+            success:options.callback
+        })
+    },
+
+    getUser_Info:function(options){
+        $.ajax({
+            url: USERINFO_GET,
+            success:options.callback
+        })
+    },
+
+    UserInfo_edit:function(options){
+        $.ajax({
+            type: 'post',
+            url: USERINFO_EDIT,
+            data:options.data,
+            contentType: false,
+            processData: false,
+            success:options.success
+        })
+    },
+
 }
